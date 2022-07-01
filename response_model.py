193dbb4d5af470d8e95404e0ebbb07369291ef6a
Kangyongsu <abc2623679@naver.com>
2022-07-01 19:22:15 +0900

    responde_model

--- response_model.py
+++ response_model.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 app = FastAPI()
 
@@ -15,6 +16,28 @@ class UserOut(BaseModel):
     full_name: str | None = None
 
 
-@app.post("/items/", response_model=UserOut)
+@app.post("/items/", response_model=UserOut, response_model_exclude_unset=False)
 async def create_item(user: UserIn):
     return user
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz",
+        "description": "There goes my baz",
+        "price": 50.2,
+        "tax": 10.5,
+    },
+}
+
+@app.get("/items/{item_id}/name",response_model=Item, response_model_include=["name", "description"])
+async def read_item_name(item_id: str):
+    return {"tt"}
